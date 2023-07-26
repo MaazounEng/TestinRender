c14a2e90d2567269cf3cc88ef4637b89925ceed1
MaazounEng <104103722+MaazounEng@users.noreply.github.com>
2023-07-26 15:48:54 +0100

    Update __main__.py

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -49,27 +49,31 @@ async def webhook(request):
 @router.register("installation", action="created")
 async def repo_installation_added(event, gh, *args, **kwargs):
     installation_id = event.data["installation"]["id"]
-
     installation_access_token = await apps.get_installation_access_token(
         gh,
         installation_id=installation_id,
         app_id=os.environ.get("GH_APP_ID"),
-        private_key=os.environ.get("GH_PRIVATE_KEY")
+        private_key=os.environ.get("GH_PRIVATE_KEY"),
     )
-    repo_name = event.data["repositories"][0]["full_name"]
-    url = f"/repos/{repo_name}/issues"
-    response = await gh.post(
-        url,
-                     data={
-        'title': 'Thanks for installing my bot',
-        'body': 'Thanks!',
+    sender_name = event.data["sender"]["login"]
+
+    for repo in event.data["repositories"]:
+
+        repo_full_name = repo["full_name"]
+        response = await gh.post(
+            f"/repos/{repo_full_name}/issues",
+            data={
+                "title": "Thanks for installing me",
+                "body": f"You're the best! @{sender_name}",
             },
-        oauth_token=installation_access_token["token"]
-                             )
-    print(response)
-
-
-
+            oauth_token=installation_access_token["token"],
+        )
+        issue_url = response["url"]
+        await gh.patch(
+            issue_url,
+            data={"state": "closed"},
+            oauth_token=installation_access_token["token"],
+        )
 if __name__ == "__main__":  # pragma: no cover
     app = web.Application()
 
